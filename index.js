115749a80f8af6d64b246bf5e272513299c92153
FZ2161 <filipzalewski44@gmail.com>
2023-09-20 06:51:51 +0200

    add missing data

--- index.js
+++ index.js
@@ -20,33 +20,29 @@ var con = mysql.createConnection({
 
 
     axios.get('https://restcountries.com/v3.1/all').then((resp) =>{
-
-
             for(var i = 0; i<= resp.data.length - 1; i++){
-
-
                 const country = resp.data[i]
-                //console.log(`${country.name.common}, ${country.population}, ${country.region}, ${country.capital}`)
+                const countryName = country.name.common
 
+                const sql2 = `SELECT * FROM kraje WHERE name = '${countryName}'`
 
-                const sql = `INSERT INTO kraje (name, population, region, capital) VALUES ('${country.name.common}', '${country.population}', '${country.region}', '${country.capital}')`
-
-
-                con.query(sql, (err, results, filds)=>{
-
+                con.query(sql2, (err, results, filds)=>{
                     if(err) console.log(err)
-                
+                    else{
+                        if (results.length==0){
+                            const sql = `INSERT INTO kraje (name, population, region, capital) VALUES ('${country.name.common}', '${country.population}', '${country.region}', '${country.capital}')`
+
+                            con.query(sql, (err, results, filds)=>{
+
+                                if(err) console.log(err)
+                                else console.log(`dodano kraj: ${countryName}`)                            
+                            })
+                        }
+                    }
                 })
-
-
-
-
             }
         })
 
 
-app.get('/', (req, res)=>{
-    res.send(`${name}`)
-})
 
 app.listen(port)
