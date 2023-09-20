8501cd8c3e36a82293e84e01e62ac4a81f2d2d1a
FZ2161 <filipzalewski44@gmail.com>
2023-09-20 09:09:34 +0200

    jh

--- index.js
+++ index.js
@@ -24,25 +24,36 @@ var con = mysql.createConnection({
                 const country = resp.data[i]
                 const countryName = country.name.common
 
-                const sql2 = `SELECT * FROM kraje WHERE name = '${countryName}'`
+                // const sql2 = `SELECT * FROM kraje WHERE name = '${countryName}'`
 
-                con.query(sql2, (err, results, filds)=>{
-                    if(err) console.log(err)
-                    else{
-                        if (results.length==0){
-                            const sql = `INSERT INTO kraje (name, population, region, capital) VALUES ('${country.name.common}', '${country.population}', '${country.region}', '${country.capital}')`
+                // con.query(sql2, (err, results, filds)=>{
+                //     if(err) console.log(err)
+                //     else{
+                //         if (results.length==0){
+                //             const sql = `INSERT INTO kraje (name, population, region, capital) VALUES ('${country.name.common}', '${country.population}', '${country.region}', '${country.capital}')`
 
-                            con.query(sql, (err, results, filds)=>{
+                //             con.query(sql, (err, results, filds)=>{
 
-                                if(err) console.log(err)
-                                else console.log(`dodano kraj: ${countryName}`)                            
-                            })
-                        }
-                    }
+                //                 if(err) console.log(err)
+                //                 else console.log(`dodano kraj: ${countryName}`)                            
+                //             })
+                //         }
+                //     }
+                // })
+
+                app.get('/kontynent/:kontynent', (req, res)=>{
+                    const kontynent = req.params.kontynent
+                    const sql3 = `SELECT * FROM kraje WHERE region = '${kontynent}'`
+                    con.query(sql3, (err, results, filds)=>{
+                        if(err) console.log(err)
+                        else res.send(results)
+                    })
                 })
             }
         })
 
+
+        
 
 
 app.listen(port)
