f91abf05c71bab72571d1bacb28423e700501868
FZ2161 <filipzalewski44@gmail.com>
2023-09-20 15:44:21 +0200

    area

--- index.js
+++ index.js
@@ -23,32 +23,52 @@ var con = mysql.createConnection({
             for(var i = 0; i<= resp.data.length - 1; i++){
                 const country = resp.data[i]
                 const countryName = country.name.common
+                const area = country.area
 
-                // const sql2 = `SELECT * FROM kraje WHERE name = '${countryName}'`
+                const sql2 = `SELECT * FROM kraje WHERE name = '${countryName}'`
 
-                // con.query(sql2, (err, results, filds)=>{
-                //     if(err) console.log(err)
-                //     else{
-                //         if (results.length==0){
-                //             const sql = `INSERT INTO kraje (name, population, region, capital) VALUES ('${country.name.common}', '${country.population}', '${country.region}', '${country.capital}')`
+                con.query(sql2, (err, results, filds)=>{
+                    if(err) console.log(err)
+                    else{
+                        // if (results.length==0){
+                        //     const sql = `INSERT INTO kraje (name, population, region, capital) VALUES ('${country.name.common}', '${country.population}', '${country.region}', '${country.capital}')`
 
-                //             con.query(sql, (err, results, filds)=>{
+                        //     con.query(sql, (err, results, filds)=>{
 
-                //                 if(err) console.log(err)
-                //                 else console.log(`dodano kraj: ${countryName}`)                            
-                //             })
-                //         }
-                //     }
+                        //         if(err) console.log(err)
+                        //         else console.log(`dodano kraj: ${countryName}`)                            
+                        //     })
+
+                        // }
+
+                        const sql4 = `UPDATE kraje SET area ='${area}'`
+                        con.query(sql4, (err, results, filds)=>{
+                            if(err) console.log(err)
+
+                        })
+                    }
+                })
+
+                
+
+                // app.get('/kontynent/:kontynent', (req, res)=>{
+                //     const kontynent = req.params.kontynent
+                //     const sql3 = `SELECT * FROM kraje WHERE region = '${kontynent}'`
+                //     con.query(sql3, (err, results, filds)=>{
+                //         if(err) console.log(err)
+                //         else res.send(results)
+                //     })
                 // })
 
-                app.get('/kontynent/:kontynent', (req, res)=>{
-                    const kontynent = req.params.kontynent
-                    const sql3 = `SELECT * FROM kraje WHERE region = '${kontynent}'`
-                    con.query(sql3, (err, results, filds)=>{
-                        if(err) console.log(err)
-                        else res.send(results)
-                    })
-                })
+
+                // app.get('/population/:population', (req, res)=>{
+                //     const population = parseInt(req.params.population)
+                //     const sql3 = `SELECT * FROM kraje WHERE population >= '${population}'`
+                //     con.query(sql3, (err, results, filds)=>{
+                //         if(err) console.log(err)
+                //         else res.send(results)
+                //     })
+                // })
             }
         })
 
